eb9db83b352471357a44a8d2acae9c7029a714d2
Stumpir <alyguda@gmail.com>
2022-03-24 01:56:39 +0300

    Initial commit

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,8 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8 { // Вывод не очень красивый получается, потому что у последнего объекта Deth8 в строчке
+}                    // не переопределен toString в исходном проекте, но так как в задании написано только
+                     // создать связь до 8 элемента, то оставил так
